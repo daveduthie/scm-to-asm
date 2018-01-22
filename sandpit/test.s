a07cf4645d0e2c65c1763a649e4b640efb04f7b3
David Duthie <dave.a.duthie@gmail.com>
2018-01-22 12:55:51 -0500

    add test.c and test.s with comments to sandpit

---
+++ sandpit/test.s
@@ -0,0 +1,42 @@
+	.text                         ; Code lives here
+	.align 4,0x90
+	.globl _scheme_entry          ; Add scheme_entry to symbol table (thus enabling linker to include it)
+_scheme_entry:                  ; scheme_entry defined here
+LFB0:
+	movl	$7, %eax                ; Move literal 7 into eax (return register)
+	ret                           ; return tears down stack frame &c. then passes control back to caller
+
+;;; No idea what any of this does
+LFE0:
+	.section __TEXT,__eh_frame,coalesced,no_toc+strip_static_syms+live_support
+EH_frame1:
+	.set L$set$0,LECIE1-LSCIE1
+	.long L$set$0
+LSCIE1:
+	.long	0
+	.byte	0x1
+	.ascii "zR\0"
+	.byte	0x1
+	.byte	0x78
+	.byte	0x10
+	.byte	0x1
+	.byte	0x10
+	.byte	0xc
+	.byte	0x7
+	.byte	0x8
+	.byte	0x90
+	.byte	0x1
+	.align 3
+LECIE1:
+LSFDE1:
+	.set L$set$1,LEFDE1-LASFDE1
+	.long L$set$1
+LASFDE1:
+	.long	LASFDE1-EH_frame1
+	.quad	LFB0-.
+	.set L$set$2,LFE0-LFB0
+	.quad L$set$2
+	.byte	0
+	.align 3
+LEFDE1:
+	.subsections_via_symbols
